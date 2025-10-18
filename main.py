57871aa415506fe3d4e247c7772d4ea3a7413b00
SUSANTxBOTS <susantpc1232@gmail.com>
2025-10-18 21:19:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("7905598523:AAF2pNKuTMsozrNu0aAEYCUPOzwnxLl9HZA")
-ADMIN_ID = int(os.getenv("8156708830"))
+ADMIN_ID = 8156708830  # default fallback
 MONGO_URL = os.getenv("mongodb+srv://HYPER-KA-BETA-KENZO:Fc1cWDUgyUlHQ9bU@cluster0.acqtsqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 CHANNEL_URL = os.getenv("https://t.me/ThronexCodex")
 SUPPORT_GROUP_URL = os.getenv("https://t.me/Thronex_CHATS")
